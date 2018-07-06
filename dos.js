29fda16a946853c1ff61328630e1be7c78071160
Zahira Barriento <40859080+ZahiraBarriento@users.noreply.github.com>
2018-07-06 12:04:35 -0300

    Segunda clase
    Ejercicios de las segunda clase

---
+++ dos.js
@@ -0,0 +1,11 @@
+function mostrar()
+{
+ 	var nombre;
+ 	var laLocalidad;
+ 
+ 	nombre=document.getElementById('elNombre').value;
+ 	laLocalidad=document.getElementById('laLocalidad').value;
+ 
+ 	alert("Usted se llama "+ nombre +" y vive en "+ laLocalidad);
+
+}
---
+++ tres.js
@@ -0,0 +1,9 @@
+function mostrar()
+{
+	var elPrecio
+	var porcentaje
+
+	elPrecio=prompt ("Ingrese su precio");
+	porcentaje=prompt("Ingrese su porcentaje");
+
+}
