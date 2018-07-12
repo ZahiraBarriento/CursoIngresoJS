1842dbc4df372300cbc864dcde6f48ac328f5606
Zahira Barriento <40859080+ZahiraBarriento@users.noreply.github.com>
2018-07-12 12:22:33 -0300

    IntroduccionIf
    Ejercicio 9

---
+++ jsInstruccionIF (9).js
@@ -0,0 +1,8 @@
+function mostrar()
+//Genero el número RANDOM entre 1 y 10 
+{
+	var numeroRandom;
+	numeroRandom=Math.floor(Math.random() * 10) + 1;
+	alert("El numero es: "+numeroRandom);
+}
+//FIN DE LA FUNCIÓN
